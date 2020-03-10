77f3dc632cf5cbbca2fb1f13d1a46ac6fbfe7dbe
Klaus Klein <klauswk1@hotmail.com>
2020-03-10 12:49:57 -0300

    Chocolatey packing working

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'sshimas.exe'
+
+$packageName = 'sshimas'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  softwareName  = 'sshimas*'
+  file          = $fileLocation
+
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
